50ae9efb9f4b9d87117a3c799817bb1066b3c0c6
FreekStelten <124879964+FreekStelten@users.noreply.github.com>
2023-04-06 15:07:01 +0200

    connected to DB en foutmelding als iets mis gaat

--- slagboom.go
+++ slagboom.go
@@ -1,35 +1,37 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
+	"database/sql"
+	"fmt"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    // Set up database connection parameters
-    dbUser := "Admin"
-    dbPass := "Fonteyn@DB"
-    dbName := "slagboom_db"
-    dbAddress := "127.0.0.1"
+	// Set up database connection parameters
+	dbUser := "Admin"
+	dbPass := "Fonteyn@DB"
+	dbName := "slagboomm_db"
+	dbAddress := "127.0.0.1"
 
-    // Create data source name (DSN)
-    dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPass, dbAddress, dbName)
+	// Create data source name (DSN)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPass, dbAddress, dbName)
 
-    // Connect to database
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
+	// Connect to database
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println("er kan geen connecting naar de database gemaakt worden:", err.Error())
+		return
+	}
+	defer db.Close()
 
-    // Ping database to ensure connection is valid
-    err = db.Ping()
-    if err != nil {
-        panic(err.Error())
-    }
+	// Ping database to ensure connection is valid
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Er kan niet gepingt worden naar de database:", err.Error())
+		return
+	}
 
-    // Connection successful
-    fmt.Println("Connected to database!")
+	// Connection successful
+	fmt.Println("Connected to database!")
 }
